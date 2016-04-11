8f48e1d9eb47dee057e554d6973fc6f162a3274d
Noopyx <quentin.porion@gmail.com>
2016-04-11 17:08:20 +0200

    pish

--- inc/user.class.php
+++ inc/user.class.php
@@ -46,7 +46,7 @@ class User extends CommonDBTM {
    public $dohistory         = true;
    public $history_blacklist = array('date_mod', 'date_sync', 'last_login',
                                      'publicbookmarkorder', 'privatebookmarkorder');
-   public $komeo = 0;
+   $komeo = 0;
 
    // NAME FIRSTNAME ORDER TYPE
    const REALNAME_BEFORE   = 0;
