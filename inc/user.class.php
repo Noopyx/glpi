97bde063d095e1670007660e1c1e8488a613339f
Noopyx <quentin.porion@gmail.com>
2016-04-11 17:16:36 +0200

    push

--- inc/user.class.php
+++ inc/user.class.php
@@ -46,7 +46,7 @@ class User extends CommonDBTM {
    public $dohistory         = true;
    public $history_blacklist = array('date_mod', 'date_sync', 'last_login',
                                      'publicbookmarkorder', 'privatebookmarkorder');
-   public $komeo;
+   public var $komeo = 0;
 
    // NAME FIRSTNAME ORDER TYPE
    const REALNAME_BEFORE   = 0;
