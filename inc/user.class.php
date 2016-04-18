e52568f66956b65db1f47158ba509edacefb3134
Noopyx <quentin.porion@gmail.com>
2016-04-18 16:38:07 +0200

    push

--- inc/user.class.php
+++ inc/user.class.php
@@ -1944,9 +1944,9 @@ class User extends CommonDBTM {
 							
 								foreach ($dropdown[$key] as $key2 => $val2) {
 									if ($myId['id_group'] == $key2)
-										echo "<option value=".$donnees['id']." selected>".$val2."</option>";
+										echo "<option value='".$donnees['id']."' selected>".$val2."</option>";
 									else
-										echo "<option value=".$donnees['id']." >".$val2."</option>";
+										echo "<option value='".$donnees['id']."' >".$val2."</option>";
 								}
 								echo "</optgroup>";
 							}
