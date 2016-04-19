2a69a8b74411499b7c2880286e976761a5dc1d27
Noopyx <quentin.porion@gmail.com>
2016-04-19 12:10:46 +0200

    push

--- inc/user.class.php
+++ inc/user.class.php
@@ -1942,6 +1942,7 @@ class User extends CommonDBTM {
 		 if ($result) {
 				 while ($donnees = $result->fetch()) {
 					 if($donnees['groups_id'] == 0) {
+						 echo "<script type=\"text/javascript\"> console.log(\"ID :".$donnees['id']." \");</script>";
 						 $result2 = $bdd->query("select * from glpi_groups where groups_id=".$donnees['id']);
 						 if($result2) {
 							$dropdown[$donnees['id']] = array();
