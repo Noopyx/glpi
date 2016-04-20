3db7f7f481b4680c50519753bcf4358f7b95e742
Noopyx <quentin.porion@gmail.com>
2016-04-20 15:07:55 +0200

    push

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4742,9 +4742,9 @@ class Ticket extends CommonITILObject {
 
                case "requestbyself" :
                default :
-                  $options['criteria'][0]['field']      = 11; // status
+                  $options['criteria'][0]['field']      = 12; // status
                   $options['criteria'][0]['searchtype'] = 'equals';
-                  $options['criteria'][0]['value']      = 'notold';
+                  $options['criteria'][0]['value']      = 'process';
                   $options['criteria'][0]['link']       = 'AND';
 
                   $options['criteria'][1]['field']      = 71; // groups_id
