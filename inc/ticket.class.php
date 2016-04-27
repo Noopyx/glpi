b51b72ebf9bec4a997f2daf755c090ee18ca1326
Noopyx <quentin.porion@gmail.com>
2016-04-27 10:47:32 +0200

    push

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4051,7 +4051,7 @@ class Ticket extends CommonITILObject {
                break;
          }
          echo "<span id='show_category_by_type t'>";
-		 self::dropdownCategory(array('action' => 'update' , 'id' => $ID ,'on_change' => 'this.form.submit()'));
+		 self::dropdownCategory(array('action' => 'update' , 'id' => $ID ,'on_change' => 'this.form.submit()', 'value' => $this->fields['itilcategories_id']));
          //ITILCategory::dropdown($opt);
          echo "</span>";
       } else {
